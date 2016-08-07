04a40672720381cf1281289dafff89ef32293d0a
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-08-06 22:49:14 -0400

    Improve build script

--- build.sh
+++ build.sh
@@ -4,11 +4,23 @@ basedir="$(cd "$(dirname "$0")"; pwd)"
 
 chroot="$basedir/chroot"
 
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+built(){
+  if (exists arch-nspawn); then
+    arch-nspawn "$chroot/$reponame" pacman -Q "$1" > /dev/null 2>&1
+  else
+    pacman -Q "$1" > /dev/null 2>&1
+  fi
+}
+
 find_deps() {
   (
   set -e
   cmd < "${basedir}/$1/$2/.SRCINFO" | sed -nr 's/^\W*depends = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r dep; do
-    if [ -d "$basedir/$1/$basedir/$dep" ]; then
+    if [ -d "$basedir/$1/$dep" ]; then
       echo $"{dep}"
     fi
   done
@@ -24,15 +36,15 @@ build() {
     echo "Cannot find PKGBUILD in $packagedir"
     return 1
   fi
-  if (arch-nspawn "$chroot/$reponame" pacman -Q "$packagename" > /dev/null 2>&1); then
-	    echo "Package $packagename already built"
-	    return
-	fi
+  if built $pkgname; then
+    echo "Package $packagename already built"
+    return
+  fi
   if [ ! -f "$packagedir/.SRCINFO" ]; then
     mksrcinfo
   fi
   find_deps "$1" "$packagename" | while read -r dep; do
-    if ! (arch-nspawn "$chroot/$reponame" pacman -Q "$dep" > /dev/null 2>&1); then
+    if built $dep; then
       build "$1" "$dep"
     fi
   done
@@ -45,7 +57,7 @@ build() {
   )
 }
 
-if [ ! -d "$chroot" ]; then
+if (exists mkarchroot) && [ ! -d "$chroot" ]; then
   mkdir "$chroot"
   mkarchroot "$chroot/root" base-devel
 fi
@@ -59,7 +71,7 @@ if [ "$#" -lt 2 ]; then
 else
   # Build only requested packages
   reponame=$1
-  for pkg in "$@"; do
+  for pkg in "$@:2"; do
     build "$reponame" "$pkg"
   done
 fi
