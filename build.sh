ba0f578c60b71b18d520dd8b649d3c9b8639a8fa
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-08-07 23:38:31 -0400

    Change build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #/usr/bin/bash
 
 basedir="$PWD"
-chroot="$basedir/chroot"
 reponame="${1:-aur}"
 
 exists() {
@@ -11,14 +10,17 @@ exists() {
   )
 }
 
-built(){
+find_pkgs() {
   (
   set -e
-  if (exists arch-nspawn); then
-    arch-nspawn "$chroot/$reponame" pacman -Q "$1" > /dev/null 2>&1
-  else
-    pacman -Q "$1" > /dev/null 2>&1
-  fi
+  cmd < "$(cd "$basedir/$reponame" && find -- * -prune -type d | tr '\n' ' ')"
+  )
+}
+
+find_pkgnames() {
+  (
+  set -e
+  cmd < "$(cd "$basedir/$reponame/$1" && makepkg --printsrcinfo | sed -nr 's/^\W*pkgname = ([-a-zA-Z0-9]+).*$/\1/p' | tr '\n' ' ')"
   )
 }
 
@@ -26,10 +28,14 @@ find_deps() {
   (
   set -e
   cd "$basedir/$reponame/$1"
-  cmd < "$(makepkg --printsrcinfo)" | sed -nr 's/^\W*depends = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r dep; do
-    if [ -d "$basedir/$reponame/$dep" ]; then
-      echo "$dep"
-    fi
+  cmd < "$(cd "$basedir/$reponame/$1" makepkg --printsrcinfo)" | sed -nr 's/^\W*depends = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r dep; do
+    for pkg in ${pkgs:@}; do
+      for name in ${"$pkg"_names:@}; do
+        if [ "$dep" == "$name" ]; then
+          echo "$pkg"
+        fi
+      done
+    done
   done
   )
 }
@@ -38,43 +44,26 @@ build() {
   (
   set -e
   packagename="$1"
-  packagedir="$basedir/$reponame/$packagename"
   if [ ! -f "$packagedir/PKGBUILD" ]; then
     echo "Cannot find PKGBUILD in $packagedir"
     return 1
   fi
-  if built "$packagename"; then
-    echo "Package $packagename already built"
-    return
-  fi
-  if [ ! -f "$packagedir/.SRCINFO" ]; then
-    makepkg --printsrcinfo > "$packagedir/.SRCINFO"
-  fi
   find_deps "$packagename" | while read -r dep; do
-    if built "$dep"; then
-      build "$dep"
-    fi
+    build "$dep"
   done
-  mkdir -p "$basedir/pkg"
-  cd $packagedir
-  if (exists mkarchroot); then
-    makechrootpkg -r "$chroot" -l "$reponame" -- -i
-    mv ./*.pkg.tar.xz "$basedir/pkg"
-  else
-    makepkg -s --noconfirm --noprogressbar
-  fi
+  cd "$packagedir"
+  makepkg -si --noconfirm --noprogressbar
   )
 }
 
-if (exists mkarchroot) && [ ! -d "$chroot" ]; then
-  mkdir "$chroot"
-  mkarchroot "$chroot/root" base-devel
-fi
-
 if [ "$#" -lt 2 ]; then
   # Build all packages from a repository (defaulting to aur)
-  cd "$basedir/$reponame"
-  find -type d | sed 's/\.\///' | tail -n +2 | while read -r pkg; do
+  pkgs=( $(cd "$basedir/$reponame" && find -- * -prune -type d | tr '\n' ' ') )
+  for pkg in ${pkgs:@}; do
+    packagedir="$basedir/$reponame/$pkg"
+    "$pkg"_names=find_pkgnames "$pkg"
+  done
+  for pkg in ${pkgs:@}; do
     build "$pkg"
   done
 else
