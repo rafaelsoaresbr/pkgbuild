3697835f6cb6cc05bde5718053c9461d50ab13ff
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-08-08 00:00:59 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -3,34 +3,13 @@
 basedir="$PWD"
 reponame="${1:-aur}"
 
-exists() {
-  (
-  set -e
-  command -v "$1" >/dev/null 2>&1
-  )
-}
-
-find_pkgs() {
-  (
-  set -e
-  cmd < "$(cd "$basedir/$reponame" && find -- * -prune -type d | tr '\n' ' ')"
-  )
-}
-
-find_pkgnames() {
-  (
-  set -e
-  cmd < "$(cd "$basedir/$reponame/$1" && makepkg --printsrcinfo | sed -nr 's/^\W*pkgname = ([-a-zA-Z0-9]+).*$/\1/p' | tr '\n' ' ')"
-  )
-}
-
 find_deps() {
   (
   set -e
   cd "$basedir/$reponame/$1"
   cmd < "$(cd "$basedir/$reponame/$1" makepkg --printsrcinfo)" | sed -nr 's/^\W*depends = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r dep; do
-    for pkg in ${pkgs:@}; do
-      for name in ${"$pkg"_names:@}; do
+    for pkg in ${pkgs[@]}; do
+      for name in ${${pkg}_names[@]}; do
         if [ "$dep" == "$name" ]; then
           echo "$pkg"
         fi
@@ -59,11 +38,11 @@ build() {
 if [ "$#" -lt 2 ]; then
   # Build all packages from a repository (defaulting to aur)
   pkgs=( $(cd "$basedir/$reponame" && find -- * -prune -type d | tr '\n' ' ') )
-  for pkg in ${pkgs:@}; do
+  for pkg in ${pkgs[@]}; do
     packagedir="$basedir/$reponame/$pkg"
-    "$pkg"_names=find_pkgnames "$pkg"
+    "$pkg"_names=( $(cd "$packagedir" && makepkg --printsrcinfo | sed -nr 's/^\W*pkgname = ([-a-zA-Z0-9]+).*$/\1/p' | tr '\n' ' ') )
   done
-  for pkg in ${pkgs:@}; do
+  for pkg in ${pkgs[@]}; do
     build "$pkg"
   done
 else
