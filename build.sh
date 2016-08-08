6cc761e2408b9cd46281108eef3bdd0132385d76
Rafael Soares <rafaelsoaresbr@gmail.com>
2016-08-08 00:22:05 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ find_deps() {
   cd "$basedir/$reponame/$1"
   cmd < "$(cd "$basedir/$reponame/$1" makepkg --printsrcinfo)" | sed -nr 's/^\W*depends = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r dep; do
     for pkg in ${pkgs[@]}; do
-      for name in ${${pkg}_names[@]}; do
-        if [ "$dep" == "$name" ]; then
+      cmd < "$(cd "$packagedir" && makepkg --printsrcinfo)" | sed -nr 's/^\W*pkgname = ([-a-zA-Z0-9]+).*$/\1/p' | while read -r package_name; do
+        if [ "$dep" == "$package_name" ]; then
           echo "$pkg"
         fi
       done
@@ -38,10 +38,6 @@ build() {
 if [ "$#" -lt 2 ]; then
   # Build all packages from a repository (defaulting to aur)
   pkgs=( $(cd "$basedir/$reponame" && find -- * -prune -type d | tr '\n' ' ') )
-  for pkg in ${pkgs[@]}; do
-    packagedir="$basedir/$reponame/$pkg"
-    "$pkg"_names=( $(cd "$packagedir" && makepkg --printsrcinfo | sed -nr 's/^\W*pkgname = ([-a-zA-Z0-9]+).*$/\1/p' | tr '\n' ' ') )
-  done
   for pkg in ${pkgs[@]}; do
     build "$pkg"
   done
